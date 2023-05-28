a6865b5794fe71d15be21e0719d0cef034d1fdfc
Ryan_Cruz <57672255+ryancruz2@users.noreply.github.com>
2023-05-28 09:10:23 -0300

    ajuste docker (#3)
    * Create README.md
    
    * ajuste docker

---
+++ src/Panda.SOS/Configuration/Controller.cs
@@ -0,0 +1,8 @@
+﻿namespace Panda.SOS.Configuration;
+public static class Controller
+{
+    public static IServiceCollection ConfigureController(this IServiceCollection services, string connectionString)
+    {
+        return services;
+    }
+}
