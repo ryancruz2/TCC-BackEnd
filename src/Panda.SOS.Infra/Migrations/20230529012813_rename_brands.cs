54c68a53995d8a9b993bcc1f558036a3e8503736
Ryan_Cruz <57672255+ryancruz2@users.noreply.github.com>
2023-06-03 16:29:07 -0300

    Docker (#7)
    * Create README.md
    
    * ajuste docker
    
    * arrumando docker file

---
+++ src/Panda.SOS.Infra/Migrations/20230529012813_rename_brands.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Panda.SOS.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class rename_brands : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "brands",
+                table: "Phones",
+                newName: "maker");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "maker",
+                table: "Phones",
+                newName: "brands");
+        }
+    }
+}
--- src/Panda.SOS/Configuration/Controller.cs
+++ src/Panda.SOS/Configuration/Controller.cs
@@ -3,6 +3,7 @@ public static class Controller
 {
     public static IServiceCollection ConfigureController(this IServiceCollection services, string connectionString)
     {
-        return services;
+      services.AddControllers();
+       return services;
     }
 }
